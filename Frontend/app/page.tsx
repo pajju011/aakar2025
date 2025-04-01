bf926cc25f48d810c1360b5a60460c761a161fbf
Prakrithi14 <mprakrithi84gmail.com>
2025-04-01 12:04:05 +0530

    glimpse

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,21 @@
+import GlimpseOfAakar from "@/components/(User)/glimpseofaakar";
 import LandingPage from "@/components/(User)/landingPage";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <div className="h-[1000vh] font-GameOfSquids">
       <LandingPage/>
+      </div>
+      <div className="h-[1000vh] font-GameOfSquids">
+      <GlimpseOfAakar/>
     </div>
+    </>
   );
+  // return (
+  //   <div className="h-[1000vh] font-GameOfSquids">
+  //     
+  //   </div>
+  // );
 }
